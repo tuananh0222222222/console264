bffb5039328bdf9769888e82fcac67a6c63d4af2
Khuất Tuấn Anhhhhhhh <75654034+tuananh0222222222@users.noreply.github.com>
2022-04-27 08:12:53 +0700

    add toan tu so hoc

--- console264/Program.cs
+++ console264/Program.cs
@@ -21,17 +21,35 @@ namespace Program
 
             // hl.addhello();
 
-           // student std = new student();
+            // student std = new student();
 
-           //std.ten = Console.ReadLine();
-           // std.tuoi = int.Parse(Console.ReadLine());
-           // std.gioitinh =Console.ReadLine();
+            //std.ten = Console.ReadLine();
+            // std.tuoi = int.Parse(Console.ReadLine());
+            // std.gioitinh =Console.ReadLine();
 
-           // std.HT();
+            // std.HT();
 
+            //double a = 12.30;
+            //int b = (int)a;
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+            //double d = 100.5;
+
+
+
+            //Console.WriteLine(Convert.ToInt32(d));
+
+            int a = -10;
+            double b = 29.999;
+
+           
+            Console.WriteLine(Math.Sqrt(b));// can bac 2
+            Console.WriteLine(Math.Abs(a));// tri tuyet doi
+            Console.WriteLine(Math.Max(a, b));// max
+            Console.WriteLine(Math.Min(a, b));// min
+            Console.WriteLine(Math.Round(b));// lam con so thap phan 1
             
-            
-            
+
         }
     }
 }
